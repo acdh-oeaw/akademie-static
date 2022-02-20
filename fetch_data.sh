72139e7a579432c9af1305bc5e6898937a2b22a7
csae8092 <peter.andorfer@oeaw.ac.at>
2022-02-20 20:19:19 +0100

    denormalize indices

--- fetch_data.sh
+++ fetch_data.sh
@@ -19,3 +19,6 @@ echo "add ids"
 add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
 add-attributes -g "./data/meta/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
 add-attributes -g "./data/indices/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
+
+echo "denormalize indices"
+denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -x ".//tei:title[@type='num']/text()"
