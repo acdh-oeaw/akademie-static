64e0e22176d125ca229791ed433b638d2acb2f65
kisram <s2kisram@uni-trier.de>
2024-04-03 14:36:01 +0200

    comment out fs

--- html/js/run.js
+++ html/js/run.js
@@ -83,6 +83,7 @@ var editor = new LoadEditor({
       active_class: "active",
       html_class: "form-select",
     },
+    /*
     fs: {
       name: "Create full size mode",
       variants: [
@@ -100,6 +101,7 @@ var editor = new LoadEditor({
       render_svg:
         "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen' viewBox='0 0 16 16'><path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg>",
     },
+    */
     fos: {
       name: "Change font size",
       variants: [
