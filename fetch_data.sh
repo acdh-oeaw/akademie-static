d5381d92009357110300363714eacaeba0d3f00b
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-13 17:29:39 +0200

    fixed messed up data dir

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "fetching transkriptions from data_repo"
-rm -rf data/editions && mkdir data/editions
-rm -rf data/indices && mkdir data/indices
-rm -rf data/meta && mkdir data/meta
+rm -rf data/ && mkdir data
 curl -LO https://github.com/fun-with-editions/akademie-data/archive/refs/heads/main.zip
 unzip main
 
@@ -13,3 +11,4 @@ mv ./akademie-data-main/data/meta/ ./data
 
 rm main.zip
 rm -rf ./akademie-data-main
+./shellscripts/dl_imprint.sh
