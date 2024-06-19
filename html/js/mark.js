b2600327cf2e9d0caf803820e6e069bb81971184
kisram <Kinga.Sramo@oeaw.ac.at>
2024-06-19 09:56:50 +0200

    adjust for # in url

--- html/js/mark.js
+++ html/js/mark.js
@@ -1,7 +1,7 @@
 var context = document.querySelector(".yes-index");
 var instance = new Mark(context);
-let url = new URL(window.location.href);
-let urlParam = new URLSearchParams(url.search);
+let markUrl = new URL(window.location.href);
+let urlParam = new URLSearchParams(markUrl.search);
 let keyword = urlParam.get("mark");
 if (keyword === null) {
   keyword = "default";
--- html/js/ts_update_url.js
+++ html/js/ts_update_url.js
@@ -16,25 +16,31 @@ setTimeout(() => {
 
 function updateHeaderUrl() {
   setTimeout(() => {
-
     var urlToUpdate = document.querySelectorAll(".ais-Hits-item h5 a");
     var tsInputVal = tsInput.value;
 
     urlToUpdate.forEach((el) => {
       var urlToUpdateHref = el.getAttribute("href");
-      if (urlToUpdateHref.includes("?mark=")) {
-        var newUrl = urlToUpdateHref.replace(
-          /&mark=\.+$/,
-          `&mark=${tsInputVal}`
-        );
-        el.setAttribute("href", newUrl);
-      } else {
-        var newUrl = `${urlToUpdateHref}?mark=${tsInputVal}`;
-        el.setAttribute("href", newUrl);
-      }
+      var url = new URL(urlToUpdateHref, window.location.origin);
+      var params = new URLSearchParams(url.search);
+      // !hash has to be at the end of the URL
+      // Remove hash (if any)
+      var hash = url.hash;
+      url.hash = '';
+
+      // Update 'mark' parameter
+      params.set('mark', tsInputVal);
+
+      // Set the new search parameters
+      url.search = params.toString();
+
+      // Add the hash back to the end of the URL
+      url.hash = hash;
+
+      // Update the href attribute with the relative URL
+      el.setAttribute("href", url.pathname + url.search + url.hash);
     });
 
-    // listenToCheckbox();
     listenToPagination();
   }, 500);
 }
