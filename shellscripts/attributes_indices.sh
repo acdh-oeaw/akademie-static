b87e115680c7db001afe57d179ea9483333883f1
csae8092 <p.andorfer@gmail.com>
2026-06-29 12:12:25 +0200

    chore: switched to uv, fixed issue in typesense script, upgrades gh-actions

--- shellscripts/attributes_indices.sh
+++ shellscripts/attributes_indices.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "add ids"
-add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
-add-attributes -g "./data/meta/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
-add-attributes -g "./data/indices/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
+uv run add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
+uv run add-attributes -g "./data/meta/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
+uv run add-attributes -g "./data/indices/*.xml" -b "https://id.acdh.oeaw.ac.at/akademieprotokolle"
 
 echo "denormalize indices"
-denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -x ".//tei:title[@type='num']/text()"
+uv run denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -x ".//tei:title[@type='num']/text()"
 
 echo "remove noteGrps from index entries in editions"
-python pyscripts/rm_notegrps.py
+uv run  pyscripts/rm_notegrps.py
